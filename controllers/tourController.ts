34fc6595fdf968668bc8fdc405904e4720fb46c6
Ashish Barthwal <125344587+Ashish13112001@users.noreply.github.com>
2026-09-20 10:01:32 +0530

    102 -- Aggrigation Pipeline: Matching and Grouping

--- controllers/tourController.ts
+++ controllers/tourController.ts
@@ -131,6 +131,41 @@ const deleteTour = async (req: Request, res: Response) => {
   }
 };
 
+const getTourStats = async (req: Request, res: Response) => {
+  try {
+    const stats = await Tour.aggregate([
+      { $match: { ratingsAverage: { $gte: 4.5 } } },
+      {
+        $group: {
+          _id: { $toUpper: '$difficulty' },
+          numTours: { $sum: 1 },
+          avgRating: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' },
+        },
+      },
+      {
+        $sort: { avgPrice: 1 },
+      },
+      // {
+      //   $match: { _id: { $ne: 'EASY' } },
+      // },
+    ]);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        stats: stats,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
 export default {
   getTour,
   getAllTours,
@@ -138,4 +173,5 @@ export default {
   updateTour,
   deleteTour,
   aliasTopTour,
+  getTourStats,
 };
--- routes/tourRouter.ts
+++ routes/tourRouter.ts
@@ -7,6 +7,8 @@ const router = express.Router(); //express.Router() --> Express ka ek separate m
 
 router.route('/top-5-cheap').get(tourController.aliasTopTour, tourController.getAllTours);
 
+router.route('/tour-stats').get(tourController.getTourStats);
+
 router
   .route('/') //.route ka benefit hai ki same path ke different HTTP methods ko ek jagah group kar sakte ho.
   .get(tourController.getAllTours)
